3ce3ab4433078c61a0da761622681853d690c782
josemalgo <jose1432@gmail.com>
2023-04-17 22:59:09 +0200

    add validateRequest helper

--- src/api/v1/helpers/index.ts
+++ src/api/v1/helpers/index.ts
@@ -1,11 +1,20 @@
 import { NextFunction, Request, Response } from 'express'
+import { validationResult } from 'express-validator'
 import { validate as isValidUUID } from 'uuid'
 
 export const wrapAsync = (fn: Function) => (req: Request, res: Response, next: NextFunction) => fn(req, res, next).catch(next)
 
 export const isValidId = (id: string): void => {
-  if (isValidUUID(id) === false) {
+  if (!isValidUUID(id)) {
     // throw new Api400Error('Invalid ObjectID')
     throw new Error('Invalid ObjectID')
+  }
+}
+
+export const validateRequest = (req: Request): void => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    // throw new Api400Error(errors.array())
+    throw new Error('Request invalid')
   }
 }
